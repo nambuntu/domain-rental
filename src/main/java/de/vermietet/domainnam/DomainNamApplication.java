13cd90ab3fe358eb7e87818cc493d35b672fa70b
Nam Nguyen <namnvhue@gmail.com>
2018-08-14 08:00:06 +0700

    init

---
+++ src/main/java/de/vermietet/domainnam/DomainNamApplication.java
@@ -0,0 +1,12 @@
+package de.vermietet.domainnam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainNamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomainNamApplication.class, args);
+	}
+}
